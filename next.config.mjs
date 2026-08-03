746ad6313856fcfea2f5bb9f54770dc7f55e2bb1
Rehxa <pichsokreaksa505@gmail.com>
2026-08-03 13:42:30 +0700

    change config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-  serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ["firebase-admin", "jose", "jwks-rsa"],
 };
 
 export default nextConfig;
