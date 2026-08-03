fac6cbdb3b492fb28d7e6cc8e40604d43c38e406
Rehxa <pichsokreaksa505@gmail.com>
2026-08-03 10:38:48 +0700

    config firebase service account

--- src/lib/firebase/admin.js
+++ src/lib/firebase/admin.js
@@ -1,12 +1,15 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccountKey from "../../../serviceAccountKey.json";
 
 const app = getApps().length
   ? getApps()[0]
   : initializeApp({
-      credential: cert(serviceAccountKey),
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
     });
 
 export const adminAuth = getAuth(app);
