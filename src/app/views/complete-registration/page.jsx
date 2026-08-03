49d0f3d4b5d05c0aa2c86c57ac4aec16f8f23bae
Rehxa <pichsokreaksa505@gmail.com>
2026-08-03 09:48:08 +0700

    fix import

--- src/app/views/complete-registration/page.jsx
+++ src/app/views/complete-registration/page.jsx
@@ -1,5 +1,4 @@
-import SignUpPage from "@/pages/signup/signup_page";
-import CompleteResgistrationPage from "@/pages/complete_register/complete_registration_page";
+import CompleteRegistrationPage from "@/pages/complete_register/complete_registration_page";
 export default function Page() {
-  return <CompleteResgistrationPage />;
+  return <CompleteRegistrationPage />;
 }
