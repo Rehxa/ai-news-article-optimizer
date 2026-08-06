118a77627f1e0b8f874abeb1f9176d4c00225a15
Rehxa <pichsokreaksa505@gmail.com>
2026-08-06 13:28:16 +0700

    fix suspense

--- src/app/views/login/page.jsx
+++ src/app/views/login/page.jsx
@@ -1,5 +1,9 @@
 import LoginPage from "@/screens/login/login_page";
 import LoginPage2 from "@/screens/login/login2_page";
 export default function Page() {
-  return <LoginPage2 />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginPage2 />
+    </Suspense>
+  );
 }
--- src/app/views/signup/page.jsx
+++ src/app/views/signup/page.jsx
@@ -1,4 +1,10 @@
-import Register from "@/screens/register/register_page";
+// import Register from "@/screens/register/register_page";
+import Signup from "@/screens/register/register2_page.jsx";
+
 export default function Page() {
-  return <Register />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Signup />
+    </Suspense>
+  );
 }
